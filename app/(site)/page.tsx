1151151dcf4bf054ffda037dd27733ce7cfe55a6
MeongGanas <farreluken@gmail.com>
2024-05-07 21:45:56 +0800

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import { FaFacebookMessenger } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <FaFacebookMessenger size={48} className="text-sky-500 mx-auto" />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
+}
